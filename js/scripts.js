e4175f5ff29f61277c5d76eaf75026df1b58b5c2
Arlen Burton and Kevin Deganos <kevin@>
2016-03-21 16:28:29 -0700

    added js to animate active button, button used to switch between hidden  content

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+  $("li#button1").click(function() {
+    $("li#button1").addClass("active");
+    $("li#button2").removeClass("active");
+    $("li#button3").removeClass("active");
+    $("div#1").removeClass("hidden");
+    $("div#2").addClass("hidden");
+    $("div#3").addClass("hidden");
+
+  });
+
+  $("li#button2").click(function() {
+    $("li#button2").addClass("active");
+    $("li#button1").removeClass("active");
+    $("li#button3").removeClass("active");
+    $("div#2").removeClass("hidden");
+    $("div#1").addClass("hidden");
+    $("div#3").addClass("hidden");
+  });
+
+  $("li#button3").click(function() {
+    $("li#button3").addClass("active");
+    $("li#button1").removeClass("active");
+    $("li#button2").removeClass("active");
+    $("div#3").removeClass("hidden");
+    $("div#1").addClass("hidden");
+    $("div#2").addClass("hidden");
+  });
+
+
+});
